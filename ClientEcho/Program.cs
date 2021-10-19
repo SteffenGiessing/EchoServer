237813c7995965b6dd7ed3cc55d751de7d1ea73a
steffensiltamiesgiessing <SteffenKea95@gmail.com>
2021-10-19 11:24:00 +0200

    pushing

--- ClientEcho/Program.cs
+++ ClientEcho/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ClientEcho
+﻿using System.Net.Sockets;
+
+namespace ClientEcho
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            var x = new TcpClient();
         }
     }
 }
